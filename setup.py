9370b07be0d21554a030380ac98e793b3204ba4c
V Jayachandra <jayachandra.v717@gmail.com>
2025-11-01 23:14:28 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="Medical_Chatbot",
+    version="0.1.0",
+    author= "Jay Chandra",
+    author_email= "jayachandra.v986@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
